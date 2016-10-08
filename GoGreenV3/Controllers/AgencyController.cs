ff73319d36d26c6bff37263d8e97dca2ae912c6b
Jessi810 <jessisibayan@gmail.com>
2016-10-08 18:15:08 +0800

    Added AgencyMVC

---
+++ GoGreenV3/Controllers/AgencyController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GoGreenV3.Models;
+
+namespace GoGreenV3.Controllers
+{
+    public class AgencyController : Controller
+    {
+        private AgencyDbContext db = new AgencyDbContext();
+
+        // GET: Agency
+        public ActionResult Index()
+        {
+            return View(db.Agencies.ToList());
+        }
+
+        // GET: Agency/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Agency agency = db.Agencies.Find(id);
+            if (agency == null)
+            {
+                return HttpNotFound();
+            }
+            return View(agency);
+        }
+
+        // GET: Agency/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Agency/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Type,Name,Address,Latitude,Longitude,Description,WebsiteUrl,Contact,Email")] Agency agency)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Agencies.Add(agency);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(agency);
+        }
+
+        // GET: Agency/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Agency agency = db.Agencies.Find(id);
+            if (agency == null)
+            {
+                return HttpNotFound();
+            }
+            return View(agency);
+        }
+
+        // POST: Agency/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Type,Name,Address,Latitude,Longitude,Description,WebsiteUrl,Contact,Email")] Agency agency)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(agency).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(agency);
+        }
+
+        // GET: Agency/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Agency agency = db.Agencies.Find(id);
+            if (agency == null)
+            {
+                return HttpNotFound();
+            }
+            return View(agency);
+        }
+
+        // POST: Agency/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Agency agency = db.Agencies.Find(id);
+            db.Agencies.Remove(agency);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ GoGreenV3/Models/Agency.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GoGreenV3.Models
+{
+    public class Agency
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Agency Type")]
+        public string Type { get; set; }
+
+        public IEnumerable<SelectListItem> Types { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Up to {0} characters only allowed")]
+        [Display(Name = "Agency")]
+        public string Name { get; set; }
+
+        [StringLength(250, ErrorMessage = "Up to {0} characters only allowed")]
+        public string Address { get; set; }
+
+        [Required]
+        [Display(Name = "Latitude")]
+        public double Latitude { get; set; }
+
+        [Required]
+        [Display(Name = "Longitude")]
+        public double Longitude { get; set; }
+
+        [StringLength(maximumLength: 1000, ErrorMessage = "Up to {0} characters only allowed")]
+        public string Description { get; set; }
+
+        [DataType(DataType.Url)]
+        public string WebsiteUrl { get; set; }
+
+        [RegularExpression(pattern: "@^[0-9]", ErrorMessage = "{0} must only be numbers")]
+        public string Contact { get; set; }
+
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+
+    public class AgencyDbContext : DbContext
+    {
+        public AgencyDbContext() : base ("AgencyConnection")
+        { }
+
+        public System.Data.Entity.DbSet<GoGreenV3.Models.Agency> Agencies { get; set; }
+    }
+}
