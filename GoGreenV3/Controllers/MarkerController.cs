694e61a50b0479f3b362187b1a610c82ec2f0534
Jessi810 <jessisibayan@gmail.com>
2016-10-08 12:05:43 +0800

    Added Marker

---
+++ GoGreenV3/Controllers/MarkerController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GoGreenV3.Models;
+
+namespace GoGreenV3.Controllers
+{
+    public class MarkerController : Controller
+    {
+        private MarkerDbContext db = new MarkerDbContext();
+
+        // GET: Marker
+        public ActionResult Index()
+        {
+            return View(db.Markers.ToList());
+        }
+
+        // GET: Marker/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MarkerModel markerModel = db.Markers.Find(id);
+            if (markerModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(markerModel);
+        }
+
+        // GET: Marker/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Marker/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Type,Latitude,Longitude,Status,Location,Description,IsControllable,IsWorking")] MarkerModel markerModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Markers.Add(markerModel);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(markerModel);
+        }
+
+        // GET: Marker/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MarkerModel markerModel = db.Markers.Find(id);
+            if (markerModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(markerModel);
+        }
+
+        // POST: Marker/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Type,Latitude,Longitude,Status,Location,Description,IsControllable,IsWorking")] MarkerModel markerModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(markerModel).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(markerModel);
+        }
+
+        // GET: Marker/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MarkerModel markerModel = db.Markers.Find(id);
+            if (markerModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(markerModel);
+        }
+
+        // POST: Marker/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            MarkerModel markerModel = db.Markers.Find(id);
+            db.Markers.Remove(markerModel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ GoGreenV3/Models/MarkerModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace GoGreenV3.Models
+{
+    public class MarkerModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Type { get; set; }
+
+        [Required]
+        public double Latitude { get; set; }
+
+        [Required]
+        public double Longitude { get; set; }
+
+        [Required]
+        public string Status { get; set; }
+
+
+        public string Location { get; set; }
+
+
+        public string Description { get; set; }
+
+
+        public bool IsControllable { get; set; }
+
+
+        public bool IsWorking { get; set; }
+    }
+
+    public class MarkerDbContext : DbContext
+    {
+        public MarkerDbContext() : base ("MarkerConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<GoGreenV3.Models.MarkerModel> Markers { get; set; }
+    }
+}
