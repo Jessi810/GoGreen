a7fda842031a498dcec044ef1ad6b586f2128cac
Jessi810 <jessisibayan@gmail.com>
2016-10-17 21:48:34 +0800

    Added default account: Email: admin@gogreen.com Password: Admin-0

--- GoGreenV3/Migrations/Configuration.cs
+++ GoGreenV3/Migrations/Configuration.cs
@@ -1,19 +1,20 @@
 namespace GoGreenV3.Migrations
 {
+    using Models;
+    using Microsoft.AspNet.Identity;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<GoGreenV3.Models.AgencyDbContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<GoGreenV3.Models.ApplicationDbContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
-            ContextKey = "GoGreenV3.Models.AgencyDbContext";
         }
 
-        protected override void Seed(GoGreenV3.Models.AgencyDbContext context)
+        protected override void Seed(GoGreenV3.Models.ApplicationDbContext context)
         {
             //  This method will be called after migrating to the latest version.
 
@@ -27,6 +28,20 @@ namespace GoGreenV3.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+
+            var passwordHash = new PasswordHasher();
+            string password = passwordHash.HashPassword("Admin-0");
+            context.Users.AddOrUpdate(u => u.UserName,
+                new ApplicationUser
+                {
+                    UserName = "admin@gogreen.com",
+                    Email = "admin@gogreen.com",
+                    PasswordHash = password,
+                    FirstName = "Default",
+                    LastName = "Admin",
+                    EmailConfirmed = true,
+                    MemberSince = DateTime.Now
+                });
         }
     }
 }
