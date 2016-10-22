32f4925f9a29bbac550f516ba57136b5f3520afa
Jessi810 <jessisibayan@gmail.com>
2016-10-22 18:25:57 +0800

    Added show marker on index page

--- GoGreenV3/Scripts/gmaps-place-locator.js
+++ GoGreenV3/Scripts/gmaps-place-locator.js
@@ -1,14 +1,19 @@
-﻿function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
+﻿var map;
+var infowindow;
+var marker;
+var geocoder;
+
+function initMapCreatePage() {
+    map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: 16.4023, lng: 120.5960 },
         zoom: 17
     });
-    var infowindow = new google.maps.InfoWindow();
-    var marker = new google.maps.Marker({
+    infowindow = new google.maps.InfoWindow();
+    marker = new google.maps.Marker({
         map: map,
         anchorPoint: new google.maps.Point(0, -29)
     });
-    var geocoder = new google.maps.Geocoder();
+    geocoder = new google.maps.Geocoder();
 
     map.addListener('click', function (e) {
         marker.setVisible(false);
@@ -60,4 +65,31 @@
         document.getElementById("Latitude").value = place.geometry.location.lat();
         document.getElementById("Longitude").value = place.geometry.location.lng();
     });
+}
+
+function initMapIndexPage() {
+    $(document).ready(function () {
+        $('#showMarkerOnMap').on('shown.bs.modal', function () {
+            google.maps.event.trigger(map, "resize");
+        });
+    });
+    
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 16.4023, lng: 120.5960 },
+        zoom: 17
+    });
+    marker = new google.maps.Marker({
+        map: map,
+        anchorPoint: new google.maps.Point(0, -29)
+    });
+}
+
+function setMarkerLocation(id, lat, lng, location) {
+    console.log("SetMarkerLocation");
+    setTimeout(function () {
+        marker.setPosition(new google.maps.LatLng(lat, lng));
+        map.setZoom(17);
+        map.setCenter(new google.maps.LatLng(lat, lng));
+    }, 500);
+    $('#showMarkerOnMapHeader').text(location);
 }
